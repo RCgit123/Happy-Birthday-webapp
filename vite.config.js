05754e3626e7a583a487c3755c653dd2dc5610d6
Raja Chowdhury <110104095+RCgit123@users.noreply.github.com>
2025-10-31 00:07:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  root: 'public',
+  plugins: [react()],
   build: {
-    outDir: '../dist', // output will be repo/dist
-    emptyOutDir: true
-  }
+    outDir: 'dist', // Netlify expects dist/
+    emptyOutDir: true,
+  },
+  base: '/', // or '/rhea-birthday-webpage/' if deploying to subpath
 })
